8b3dce01cecfaf1a84632d1ed43f043c5d0fde85
thaibhoang <hoangthai.ftu53@gmail.com>
2024-03-23 09:12:30 +0700

    Add search form for Flickr id

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def index 
+    if params[:flickr_id]
+      
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "static_pages#index"
+  resources :static_pages
 end
